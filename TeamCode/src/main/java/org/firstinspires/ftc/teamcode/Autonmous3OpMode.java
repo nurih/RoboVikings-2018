0171562274da8e49d4dc0f3fed89704080c44e20
Nuri <RoboVikings9887@domain.com>
2018-11-03 15:40:13 -0700

    Autonomous 3 using motor unwind string

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous3OpMode.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name = "Autonomous 3 Unwind Motor")
+public class Autonmous3OpMode extends LinearOpMode {
+    private static final int LATCH_LOWERING_TICKS = 1000;
+    Servo latchLockServo;
+    AutonmousDrive drive;
+    private DcMotor latchLowerMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new AutonmousDrive(hardwareMap, telemetry);
+        latchLockServo = getLatchLockServo();
+        latchLowerMotor = RobotPart.latchLowerMotor.getInstance(hardwareMap);
+
+        waitForStart();
+
+        // unlock the latch
+        latchLockServo.setPosition(0.5);
+        sleep(3000);
+        telemetry.addLine("turning");
+
+        // unwind lowering motor.
+        double now;
+        do {
+            now = getRuntime();
+            latchLowerMotor.setPower(-1);
+        } while (getRuntime() < now + 1.5);
+
+        drive.turn(45);
+        telemetry.addLine("turned");
+        sleep(3000);
+    }
+
+
+    private Servo getLatchLockServo() {
+        latchLockServo = RobotPart.latchLockServo.getInstance(hardwareMap);
+        latchLockServo.setPosition(0);
+        return latchLockServo;
+    }
+
+
+}
