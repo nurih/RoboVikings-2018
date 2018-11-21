988390fdf06f59035aa3e74d8d851b806707ca5a
Nuri <chip.marklar@gmail.com>
2018-11-21 15:49:29 -0800

    Magnetic limit switch. Stop autonomous 3 when reaches switch.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMagneticTouchSensorOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test Magnetic Sensor")
+public class TestMagneticTouchSensorOpMode extends OpMode {
+    RevTouchSensor sensor;
+
+    @Override
+    public void init() {
+        sensor= RobotPart.latchExtenderLimitSwitch.getInstance(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (sensor.isPressed()) {
+            telemetry.addLine("It is pressed");
+        }
+        else {
+            telemetry.addLine("not pressed");
+        }
+
+    }
+}
